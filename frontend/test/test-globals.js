b806e4dcaf82d11c5bc6030e0d49dac4ef834408
Evan <evan@evanjp.com>
2020-09-14 19:42:51 -0700

    separate dev and prod webpack configs

--- frontend/test/test-globals.js
+++ frontend/test/test-globals.js
@@ -1,3 +1,3 @@
-var thermostatGlobals = {
+const thermostatGlobals = {
   ledEnabled: true,
 };
---
+++ frontend/webpack.dev.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    path: path.join(__dirname, "test"),
+    filename: "[name].bundle.js",
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "test"),
+  },
+});
