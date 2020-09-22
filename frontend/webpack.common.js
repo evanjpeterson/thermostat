a42f630e7676eedb6fa1d52a8af72225ad064450
Evan <evan@evanjp.com>
2020-09-21 22:49:26 -0700

    update webpack module resolve rules to include 'src'

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
+console.log(path.resolve(__dirname, "src"));
 module.exports = {
   entry: {
     "thermostat-control": path.join(__dirname, "src/index.jsx"),
@@ -11,6 +12,7 @@ module.exports = {
         exclude: /node_modules/,
         resolve: {
           extensions: [".js", ".jsx"],
+          modules: ["node_modules", "src"],
         },
         use: {
           loader: "babel-loader",
